990966faeb9b7650204b521c39a349dfdacdcb22
Usha13 <62227513+Usha13@users.noreply.github.com>
2020-12-04 20:23:40 +0530

    commit

--- admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-session_destroy();
-?>
-
-<script>
- window.open("index.php","_self");
- </script>
--- connect.php
+++
@@ -1,9 +0,0 @@
- <?php
-// $con=mysqli_connect("localhost",'id15504084_admin','8619771809u@A',"id15504084_dm");
-$con=mysqli_connect("localhost",'root','',"dm");
-session_start();
-   
-
-
-
-?> 
