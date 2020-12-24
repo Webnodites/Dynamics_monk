2be91f2bbdb4d3489a5a8ce4c8d2330586883a23
Usha13 <62227513+Usha13@users.noreply.github.com>
2020-12-24 11:49:43 +0530

    cm

--- contactmail.php
+++ contactmail.php
@@ -1,6 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<style>
+        .swal-text {
+            font-size: 18px;
+            text-align:center;
+        }
+        .swal-title {
+            color: #D35B0B;
+            font-size:28px;
+        }
+        .swal-button {
+            background-color: #D35B0B;
+            font-size:16px;
+        }
+        .swal-button:hover {
+            background-color: #D35B0B;
+        }
+        .swal-button:focus {
+            background-color: #D35B0B;
+        }
+        .swal-button:not([disabled]):hover {
+            background-color: #D35B0B;
+        }
+    </style>
 	<title></title>
 	<link rel="stylesheet" href="sweetalert2.min.css">
 </head>
@@ -20,19 +43,21 @@ if(isset($_POST["btnSubmit"]))
 	$phone=$_POST["txtPhone"];
 	$company=$_POST["txtCCompany"];
 	$msg=$_POST["txtCMessage"];
+	$name = $First_name." ".$Last_name;
 	
-	$from = "Client Contact";
+	$from = "DM website";
 	$webmaster = "info@dynamicsmonk.com";
 	$to="info@dynamicsmonk.com";
-	$sub = $subject;
+	$sub = "Contact";
 
 	$header = "From:".$from."<".$webmaster.">\n";
 	$message = "Name :".$name."\n";
 	$message .= "Email :".$email."\n";
 	$message .= "Mobile No. :".$phone."\n";
+	$message .= "Company :".$company."\n";
 	$message .= "Message :".$msg."\n";
      
-     echo $message;
+     // echo $message;
 	$sendmail = mail($to,$sub,$message,$header);
 	if($sendmail){
 		echo "<script>swal({
@@ -41,7 +66,7 @@ if(isset($_POST["btnSubmit"]))
 				  icon: 'success',
 				  button: 'Ok!',
 				}).then(function(){
-			         window.location.href='index.html';
+			         window.location.href='index';
 			         });
 			   </script>";
 	}
@@ -54,7 +79,58 @@ if(isset($_POST["btnSubmit"]))
 				  icon: 'error',
 				  button: 'Ok!',
 				}).then(function(){
-			         window.location.href='contact.html';
+			         window.location.href='contact';
+			         });  
+			</script>";
+
+		}
+
+}
+
+
+if(isset($_POST["esubmit"]))
+{
+	$name=$_POST["name"];
+	$email=$_POST["email"];
+	$phone=$_POST["phone"];
+	$company=$_POST["cmpny"];
+	$msg=$_POST["msg"];
+	
+	$from = "DM website";
+	$webmaster = "info@dynamicsmonk.com";
+	$to="info@dynamicsmonk.com";
+	$sub = "Quick Enquiry";
+
+	$header = "From:".$from."<".$webmaster.">\n";
+	$message = "Name :".$name."\n";
+	$message .= "Email :".$email."\n";
+	$message .= "Mobile No. :".$phone."\n";
+	$message .= "Company :".$company."\n";
+	$message .= "Message :".$msg."\n";
+     
+     // echo $message;
+	$sendmail = mail($to,$sub,$message,$header);
+	if($sendmail){
+		echo "<script>swal({
+				  title: 'Thank You!',
+				  text: 'Message Sent Successfully!',
+				  icon: 'success',
+				  button: 'Ok!',
+				}).then(function(){
+			         window.location.href='index';
+			         });
+			   </script>";
+	}
+	else{
+	
+		echo "<script>
+			swal({
+				  title: 'Oops!',
+				  text: 'Something Went Wrong!',
+				  icon: 'error',
+				  button: 'Ok!',
+				}).then(function(){
+			         window.location.href='index';
 			         });  
 			</script>";
 
